3d8cd9248429a98ed3ce8ea64ec8a49e1c1a3b4d
Albert <Albert.Piikov@gmail.com>
2025-03-06 20:12:15 +0200

    point parandus

--- src/oo/hide/Point.java
+++ src/oo/hide/Point.java
@@ -11,21 +11,23 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
     @Override
     public String toString() {
         return String.format("(%d, %d)", x, y);
     }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-        if (obj == null || !(obj instanceof Point)) {
-            return false;
+        if (obj instanceof Point other) {
+            return this.x == other.x && this.y == other.y;
         }
-        Point other = (Point) obj;
-        return this.x == other.x && this.y == other.y;
+        return false;
     }
+
     @Override
     public int hashCode() {
         return 0;
