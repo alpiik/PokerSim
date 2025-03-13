0fe236fdcbeb3a29065a51193ec8c4c1eaefb2a9
Albert <Albert.Piikov@gmail.com>
2025-03-13 21:24:26 +0200

    parandused

--- src/exceptions/numbers/ReadPropertiesFileExample.java
+++ src/exceptions/numbers/ReadPropertiesFileExample.java
@@ -1,6 +1,7 @@
 package exceptions.numbers;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -12,35 +13,21 @@ public class ReadPropertiesFileExample {
 
     private static final Logger LOGGER = Logger.getLogger(ReadPropertiesFileExample.class.getName());
 
-    public static void main(String[] args) {
-        String filePath = "src/exceptions/numbers/numbers_en.properties";
-
+    public static Properties loadProperties(String filePath) throws MissingLanguageFileException, BrokenLanguageFileException {
         Properties properties = new Properties();
-        FileInputStream is = null;
 
-        try {
-            is = new FileInputStream(filePath);
-            InputStreamReader reader = new InputStreamReader(is, StandardCharsets.ISO_8859_1);
+        try (FileInputStream is = new FileInputStream(filePath);
+             InputStreamReader reader = new InputStreamReader(is, StandardCharsets.ISO_8859_1)) {
+
             properties.load(reader);
+            return properties;
+
+        } catch (FileNotFoundException e) {
+            LOGGER.log(Level.SEVERE, "Language file not found: " + filePath);
+            throw new MissingLanguageFileException("Language file not found: " + filePath, e);
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Error reading properties file: " + e.getMessage(), e);
-        } finally {
-            close(is);
-        }
-
-        System.out.println(properties.containsKey(String.valueOf(1)));
-        System.out.println(properties.getProperty(String.valueOf(1)));
-    }
-
-    private static void close(FileInputStream is) {
-        if (is == null) {
-            return;
-        }
-
-        try {
-            is.close();
-        } catch (IOException e) {
-            LOGGER.log(Level.WARNING, "Failed to close FileInputStream: " + e.getMessage(), e);
+            LOGGER.log(Level.SEVERE, "Error reading language file: " + filePath);
+            throw new BrokenLanguageFileException("Error reading language file: " + filePath, e);
         }
     }
 }
