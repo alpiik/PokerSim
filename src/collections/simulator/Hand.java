c8956728e865ea17d594d28e017f871edd1e816d
Albert <Albert.Piikov@gmail.com>
2025-03-29 20:16:35 +0200

    parandus

--- src/collections/simulator/Hand.java
+++ src/collections/simulator/Hand.java
@@ -16,34 +16,46 @@ public class Hand implements Iterable<Card>, Comparable<Hand> {
     }
 
     public HandType getHandType() {
-        final Map<Card.CardValue, Integer> valueCounts = getValueCounts();
-        final List<Integer> counts = getSortedCounts(valueCounts);
-
-        if (isFlush() && isStraight()) {
+        if (isStraightFlush()) {
             return HandType.STRAIGHT_FLUSH;
         }
-        if (hasCount(counts, 4)) {
+        else if (isFourOfAKind()) {
             return HandType.FOUR_OF_A_KIND;
         }
-        if (isFullHouse(counts)) {
+        else if (isFullHouse()) {
             return HandType.FULL_HOUSE;
         }
-        if (isFlush()) {
+        else if (isFlush()) {
             return HandType.FLUSH;
         }
-        if (isStraight()) {
+        else if (isStraight()) {
             return HandType.STRAIGHT;
         }
-        if (hasCount(counts, 3)) {
+        else if (isTrips()) {
             return HandType.TRIPS;
         }
-        if (isTwoPairs(counts)) {
+        else if (isTwoPairs()) {
             return HandType.TWO_PAIRS;
         }
-        if (hasCount(counts, 2)) {
+        else if (isOnePair()) {
             return HandType.ONE_PAIR;
         }
-        return HandType.HIGH_CARD;
+        else {
+            return HandType.HIGH_CARD;
+        }
+    }
+
+    private boolean isStraightFlush() {
+        return isFlush() && isStraight();
+    }
+
+    private boolean isFourOfAKind() {
+        return getValueCounts().values().stream().anyMatch(count -> count == 4);
+    }
+
+    private boolean isFullHouse() {
+        Map<Card.CardValue, Integer> counts = getValueCounts();
+        return counts.containsValue(3) && counts.containsValue(2);
     }
 
     private boolean isFlush() {
@@ -54,29 +66,37 @@ public class Hand implements Iterable<Card>, Comparable<Hand> {
         if (cards.size() != 5) {
             return false;
         }
-
         List<Card> sorted = getSortedCards();
-
-        boolean normalStraight = true;
-        for (int i = 1; i < sorted.size(); i++) {
-            if (sorted.get(i).getValue().ordinal() != sorted.get(i-1).getValue().ordinal() + 1) {
-                normalStraight = false;
-                break;
-            }
-        }
-        if (normalStraight) {
-            return true;
-        }
-
-        return isWheelStraight(sorted);
+        return isNormalStraight(sorted) || isWheelStraight(sorted);
     }
 
-    private boolean isWheelStraight(List<Card> sortedCards) {
-        return sortedCards.get(0).getValue() == Card.CardValue.S2 &&
-                sortedCards.get(1).getValue() == Card.CardValue.S3 &&
-                sortedCards.get(2).getValue() == Card.CardValue.S4 &&
-                sortedCards.get(3).getValue() == Card.CardValue.S5 &&
-                sortedCards.get(4).getValue() == Card.CardValue.A;
+    private boolean isNormalStraight(List<Card> sorted) {
+        for (int i = 1; i < sorted.size(); i++) {
+            if (sorted.get(i).getValue().ordinal() != sorted.get(i-1).getValue().ordinal() + 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isWheelStraight(List<Card> sorted) {
+        return sorted.get(0).getValue() == Card.CardValue.S2 &&
+                sorted.get(1).getValue() == Card.CardValue.S3 &&
+                sorted.get(2).getValue() == Card.CardValue.S4 &&
+                sorted.get(3).getValue() == Card.CardValue.S5 &&
+                sorted.get(4).getValue() == Card.CardValue.A;
+    }
+
+    private boolean isTrips() {
+        return getValueCounts().values().stream().anyMatch(count -> count == 3);
+    }
+
+    private boolean isTwoPairs() {
+        return getValueCounts().values().stream().filter(count -> count == 2).count() == 2;
+    }
+
+    private boolean isOnePair() {
+        return getValueCounts().values().stream().anyMatch(count -> count == 2);
     }
 
     private Map<Card.CardValue, Integer> getValueCounts() {
@@ -87,30 +107,36 @@ public class Hand implements Iterable<Card>, Comparable<Hand> {
         return counts;
     }
 
-    private List<Integer> getSortedCounts(Map<Card.CardValue, Integer> valueCounts) {
-        List<Integer> counts = new ArrayList<>(valueCounts.values());
-        counts.sort(Collections.reverseOrder());
-        return counts;
-    }
-
-    private boolean hasCount(List<Integer> counts, int target) {
-        return !counts.isEmpty() && counts.get(0) == target;
-    }
-
-    private boolean isFullHouse(List<Integer> counts) {
-        return counts.size() >= 2 && counts.get(0) == 3 && counts.get(1) == 2;
-    }
-
-    private boolean isTwoPairs(List<Integer> counts) {
-        return counts.size() >= 2 && counts.get(0) == 2 && counts.get(1) == 2;
-    }
-
     private List<Card> getSortedCards() {
         return cards.stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
 
+    @Override
+    public int compareTo(Hand other) {
+        int typeCompare = this.getHandType().compareTo(other.getHandType());
+        if (typeCompare != 0) {
+            return typeCompare;
+        }
+
+        List<Card> thisSorted = this.getSortedCards();
+        List<Card> otherSorted = other.getSortedCards();
+        if (this.getHandType() == HandType.STRAIGHT) {
+            boolean thisWheel = isWheelStraight(thisSorted);
+            boolean otherWheel = isWheelStraight(otherSorted);
+            if (thisWheel && !otherWheel) return -1;
+            if (!thisWheel && otherWheel) return 1;
+        }
+        for (int i = thisSorted.size() - 1; i >= 0; i--) {
+            int cardCompare = thisSorted.get(i).compareTo(otherSorted.get(i));
+            if (cardCompare != 0) {
+                return cardCompare;
+            }
+        }
+
+        return 0;
+    }
     public boolean contains(Card card) {
         return cards.contains(card);
     }
@@ -122,32 +148,5 @@ public class Hand implements Iterable<Card>, Comparable<Hand> {
     @Override
     public Iterator<Card> iterator() {
         return cards.iterator();
-    }
-
-    @Override
-    public int compareTo(Hand other) {
-        int typeCompare = this.getHandType().compareTo(other.getHandType());
-        if (typeCompare != 0) {
-            return typeCompare;
-        }
-        List<Card> thisSorted = this.getSortedCards();
-        List<Card> otherSorted = other.getSortedCards();
-        if (this.getHandType() == HandType.STRAIGHT) {
-            boolean thisWheel = isWheelStraight(thisSorted);
-            boolean otherWheel = isWheelStraight(otherSorted);
-
-            if (thisWheel && !otherWheel) {
-                return -1;
-            } else if (!thisWheel && otherWheel) {
-                return 1;
-            }
-        }
-        for (int i = thisSorted.size() - 1; i >= 0; i--) {
-            int cardCompare = thisSorted.get(i).compareTo(otherSorted.get(i));
-            if (cardCompare != 0) {
-                return cardCompare;
-            }
-        }
-        return 0;
     }
 }
